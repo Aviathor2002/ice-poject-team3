6457e294c665c1eb96ac4cc454c91122f5f9a956
Skynet4241 <skynet4241@gmail.com>
2022-09-17 22:19:38 +0300

    modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -71,3 +71,18 @@ document.addEventListener('DOMContentLoaded', function () {
     this.classList.remove('active');
   });
 }); // end ready
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
