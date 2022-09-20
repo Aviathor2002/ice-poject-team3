dcf34480d3d6865de6817271249b73ae65d74d63
Aviathor2002 <109354604+Aviathor2002@users.noreply.github.com>
2022-09-20 10:52:05 +0000

    Deploying to gh-pages from @ Aviathor2002/ice-poject-team3@e9b78434157f531e406d9dfb1b811f0d55c55b1b 🚀

--- index.fa6cd913.js
+++
@@ -1,2 +0,0 @@
-$(window).width()>992&&$(".hero").mousemove((function(o){var e=0+1*o.pageX/40,a=0+-1*o.pageY/1110;$(".hero__foto--ice").css("transform","translate("+-e+"px, "+-a+"px)")}));
-//# sourceMappingURL=index.fa6cd913.js.map
